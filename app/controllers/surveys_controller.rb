e55e614281a84aca8051199f45035378813a6088
Joe Bernardi <joebernardi@hotmail.com>
2017-01-04 13:41:19 -0600

    create survey no questions

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -8,6 +8,36 @@ class SurveysController < ApplicationController
 
 		@survey = Survey.new
 
+
+	end
+
+	def create
+
+		@survey = Survey.new( whitelisted_survey_params )
+
+		if @survey.save
+
+			flash[:success] = "created survey"
+
+			redirect_to surveys_path
+
+		else
+
+			flash.now[:error] = "Failed to create survey!"
+			render :new
+
+		end
+
 	end
 
 end
+
+
+
+private
+
+  def whitelisted_survey_params
+
+    params.require(:survey).permit( :title,
+              :description )
+  end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,9 @@
 class Survey < ApplicationRecord
 
+	validates :title, presence: true
+	validates :description, presence: true
+
+
 	has_many :questions
 	has_many :answers
 	has_many :options
