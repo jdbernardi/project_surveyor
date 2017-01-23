654805b82efc3cb21deace8ab251c55feac718c1
Joe Bernardi <joebernardi@hotmail.com>
2017-01-23 14:37:31 -0600

    survey reroute

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,8 +1,8 @@
 class Option < ApplicationRecord
 
 
-	belongs_to :multiple_choice_questions
-	belongs_to :range_questions
+	belongs_to :multiple_choice_question, class_name: "MultipleChoiceQuestion", foreign_key: "question_id"
+	belongs_to :range_question, foreign_key: "question_id"
 
 	belongs_to :survey
 
