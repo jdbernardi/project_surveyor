4d45bd3bddfcac9935b12cb8af19679817a78653
Joe Bernardi <joebernardi@hotmail.com>
2017-01-24 22:17:29 -0600

    branch out

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -9,9 +9,6 @@ class Survey < ApplicationRecord
 	has_many :multiple_choice_questions
 	has_many :range_questions
 
-
-
-
 	#has_many :options, through: :multiple_choice_questions
 	#has_many :options, through: :multiple_choice_questions
 
