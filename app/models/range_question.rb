48d6a547d06d8f89f842a523799c604c36fd48fb
Joe Bernardi <joebernardi@hotmail.com>
2017-01-30 20:54:17 -0600

    remove collection

--- app/models/range_question.rb
+++ app/models/range_question.rb
@@ -1,7 +1,7 @@
 class RangeQuestion < ApplicationRecord
 
 	belongs_to :survey
-	has_many :options,  :as => :question
+	has_many :options,  :as => :questionable
 	belongs_to :answer
 
 	accepts_nested_attributes_for :options
