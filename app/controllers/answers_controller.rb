caeede45f3d411c823bd746d4b0d5be5afe38e02
Joe Bernardi <joebernardi@hotmail.com>
2017-02-01 13:22:23 -0600

    prep for answer build

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,28 @@
 class AnswersController < ApplicationController
 
+	include AnswersHelper
+
 	def create
 
-byebug
+		radio_responses = params[:option]
+
+		create_answers( radio_responses )
 
 	end
 
 
 end
+
+
+private
+
+
+def white_list_params
+
+	#permitting radio_mc_op????
+	params.permit( [:radio_mc_op => {} , :checkbox_op_ids => [] ], :survey_id)
+
+end
 =begin
-<ActionController::Parameters {"utf8"=>"✓", "_method"=>"patch", "authenticity_token"=>"dDTJoLv73dX/i0fGnOwccCPyx2E9gTTzj3vCu1DKN49wmIcSTu/T3irFrVBH1Hqi0ur4VwchqlfRt9XaLcmUvg==", "option"=>{"262"=>"389"}, "survey"=>{"multiple_choice_question"=>{"option_ids"=>["", "392", "394", "", "397", "", "402"]}}, "commit"=>"Finish Survey", "controller"=>"surveys", "action"=>"update", "id"=>"234"} permitted: false>
+<ActionController::Parameters {"utf8"=>"✓", "_method"=>"patch", "authenticity_token"=>"o939wheZdqWdL6CEsvGEQ8MA7/Bp97l+qfG/llmj2VKOehAj3FRptvNu99t3mTUsFYuYikaSsjxvPmzSwhIz4Q==", "radio_mc_op"=><ActionController::Parameters {"241"=>"327", "242"=>"329"} permitted: true>, "checkbox_op_ids"=>["330", "337", "338"], "commit"=>"Finish Survey", "survey_id"=>"230", "controller"=>"answers", "action"=>"create"} permitted: true>
 =end
--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,21 @@
 module AnswersHelper
+
+
+	def create_answers_for_radio( responses )
+
+		responses.each_pair do | k, v |
+
+			byebug
+
+		end
+
+	end
+
+
+	def create_answers_for_checkboxes
+
+
+	end
+
+
 end
